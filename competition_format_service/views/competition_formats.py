b582ccf78f4fcd17ae3208862f442d7917b57c05
Stig B. Dørmænen <stigbd@gmail.com>
2022-11-22 17:51:54 +0100

    Tilganger for event-admin Fixes #5

--- competition_format_service/views/competition_formats.py
+++ competition_format_service/views/competition_formats.py
@@ -65,7 +65,7 @@ class CompetitionFormatsView(View):
         db = self.request.app["db"]
         token = extract_token_from_request(self.request)
         try:
-            await UsersAdapter.authorize(token, roles=["admin"])
+            await UsersAdapter.authorize(token, roles=["admin", "event-admin"])
         except Exception as e:
             raise e from e
 
@@ -140,7 +140,7 @@ class CompetitionFormatView(View):
         db = self.request.app["db"]
         token = extract_token_from_request(self.request)
         try:
-            await UsersAdapter.authorize(token, roles=["admin"])
+            await UsersAdapter.authorize(token, roles=["admin", "event-admin"])
         except Exception as e:
             raise e from e
 
@@ -181,7 +181,7 @@ class CompetitionFormatView(View):
         db = self.request.app["db"]
         token = extract_token_from_request(self.request)
         try:
-            await UsersAdapter.authorize(token, roles=["admin"])
+            await UsersAdapter.authorize(token, roles=["admin", "event-admin"])
         except Exception as e:
             raise e from e
 
